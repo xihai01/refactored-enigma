465b8d4ec46d329f7acdd374647a2df48b7c0286
Xihai Luo <xihai.luo01@gmail.com>
2022-05-14 20:58:19 -0400

    comfigure axios get request to fetch businesses

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchResults = async function () {
+    const response = await yelp.get("/search", {
+      params: {
+        term: term,
+        limit: 50,
+        location: "toronto",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmitted={() => console.log("Term was submitted")}
+        onTermChange={setTerm}
+        onTermSubmitted={searchResults}
       />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results based on your search</Text>
     </View>
   );
 };
